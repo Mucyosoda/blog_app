ce9a0e4dc81f87f52183be4bd0f33f0259d4adcd
Mucyosoda <mucyosofa@gmail.com>
2022-03-30 14:44:41 +0100

    update migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # get 'users/index'
-  # resources :posts
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
-  end
-
-  # root to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  root "users#index"
 end
